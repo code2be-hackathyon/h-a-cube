2c2aedec54d1bb306ccbe2c0740570021c144d91
Arthur LR <contact.arthurleray@gmail.com>
2019-12-01 18:25:32 +0100

    Completed middleware IsAdmin

---
+++ resources/views/error_401.blade.php
@@ -0,0 +1,13 @@
+@extends('adminlte::page')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Cette page n'existe pas</div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
